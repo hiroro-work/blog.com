b774e295a3f6fc538ff168cf95965f6ea0856f4e
hiroro-work <hiroro.work@gmail.com>
2018-08-02 23:06:59 +0000

    change db from sqlite3 to postgre

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2018_07_22_053444) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "blogs", force: :cascade do |t|
     t.string "title"
     t.datetime "created_at", null: false
