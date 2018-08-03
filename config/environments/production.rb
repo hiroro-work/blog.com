788b6bf6b704d66a46468883966061a1f41a5422
hiroro-work <hiroro.work@gmail.com>
2018-08-03 07:47:07 +0000

    update environments/production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,7 +72,7 @@ Rails.application.configure do
   config.action_mailer.default_url_options = { host: 'agile-journey-70689.herokuapp.com' }
   config.action_mailer.smtp_settings = {
     user_name: ENV['SENDGRID_USERNAME'],
-    password : ENV['SENDGRID_PASSWORD'],
+    password: ENV['SENDGRID_PASSWORD'],
     domain: 'herokuapp.com',
     address: 'smtp.sendgrid.net',
     port: 587,
